5452dae12963dcf32430bd09657c8af6837d6778
learningday404 <97217171+learningday404@users.noreply.github.com>
2022-05-16 14:17:39 +0800

    Add files via upload

---
+++ Kordan Project/script.js
@@ -0,0 +1,5 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const slider = document.getElementById("slider");
